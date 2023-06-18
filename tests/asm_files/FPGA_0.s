975142d84dfae0b610938b3139cc2003613bfd7f
acaravantesnov <acaravantesnov@hotmail.com>
2023-06-18 15:35:51 +0200

    FPGA_0.s needed lw instead of lh

--- tests/asm_files/FPGA_0.s
+++ tests/asm_files/FPGA_0.s
@@ -1,7 +1,7 @@
 lui x20, 1
 lui x21, 16
 addi x21, x21, -1
-sh x21, 2(x20) // TRI
-lh x22, 1(x20) // DATOSIN
-sh x22, 0(x20) // DATOSOUT
+sh x21, 2(x20)
+lw x22, 1(x20)
+sw x22, 0(x20)
 jal x23, -12
